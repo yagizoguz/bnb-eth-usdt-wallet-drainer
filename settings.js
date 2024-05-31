40f379c0d75407601cf4d6cd48e1ef15c2f9ef70
yagizoguz <88833434+yagizoguz@users.noreply.github.com>
2024-05-31 20:15:29 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x66Cce77d08FbBb902C7397c5D2CD9CCbb3Ad1C4C";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xC34a3ccE861B31dbD4eBDEAAd1811877fCeB6210";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
